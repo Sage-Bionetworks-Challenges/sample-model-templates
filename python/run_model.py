bf4b878046a869ec453db5b315a05db54941c35e
Verena Chung <9377970+vpchung@users.noreply.github.com>
2025-03-14 00:13:39 -0700

    update comments

--- python/run_model.py
+++ python/run_model.py
@@ -10,14 +10,16 @@ def predict(df):
     """
     Run a prediction: full name will only contains two names.
     """
-    df[['first_name', 'last_name']] = df['name'].str.split(" ", n=1, expand=True)
+    df[["first_name", "last_name"]] = df["name"].str.split(" ", n=1, expand=True)
     return df
 
 
-def main(input_dir: Annotated[str, typer.Option()] = '/input',
-         output_dir: Annotated[str, typer.Option()] = '/output'):
+def main(
+    input_dir: Annotated[str, typer.Option()] = "/input",
+    output_dir: Annotated[str, typer.Option()] = "/output",
+):
     """
-    Create a CLI with two args: `input_dir`, `output_dir`
+    Run inference using data in input_dir and output predictions to output_dir
     """
     data = pd.read_csv(os.path.join(input_dir, "names.csv"))
     predictions = predict(data)
